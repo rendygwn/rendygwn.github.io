23220daf50a57c9df4a546cf73dbed976baecdbb
rendygwn <wanieunpisun16@gmail.com>
2023-01-30 02:48:55 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,375 @@
+<!-- <?php $url = file_get_contents('https://data.covid19.go.id/public/api/prov.json');
+      $data['list_data'] = json_decode($url, true);
+      ?> -->
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+  <!-- My Fonts-->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Krona+One&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Krona+One&family=Poppins:wght@500&display=swap" rel="stylesheet">
+
+  <!-- Font Awesome -->
+  <script src="https://kit.fontawesome.com/a122c810b1.js" crossorigin="anonymous"></script>
+
+  <!-- My CSS-->
+  <link rel="stylesheet" href="style.css">
+  <!-- Animate -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+  <!-- AOS -->
+  <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+  <title>Diet Sehat!</title>
+</head>
+
+<body>
+  <!--Navbar-->
+  <nav class="navbar navbar-expand-lg navbar-light animate__animated animate__fadeInDown">
+    <div class="container">
+      <a class="navbar-brand" href="#">Healthy <br> DietStyle</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ml-auto">
+          <a class="nav-item nav-link active" href="index.php">Home</a>
+          <a class="nav-item nav-link" href="tips.html">Tips Diet</a>
+          <a class="nav-item nav-link" href="menu.html">Menu Diet</a>
+          <a class="nav-item nav-link" href="#promotion">Promotion</a>
+          <a class="nav-item nav-link" href="#tentangkami">Tentang Kami</a>
+          <form class="d-flex">
+            <input class="form-control me-2 form" type="search" placeholder=" Search. . . " aria-label="Search">
+            <i class="fa fa-search mt-2 icon"></i>
+          </form>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <!-- Akhir Navbar -->
+
+  <!-- Jumbotron -->
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4 animate__animated animate__fadeInUp animate__delay-1s"><span>Tips</span> menurunkan berat <br> badan & <span>Menu</span> diet sehat </h1>
+      <button type="submit" class="btn animate__animated animate__fadeInUp animate__delay-1s"><a href="login.html" style="color: #fff;">Join Us</a></button>
+    </div>
+  </div>
+  <!-- Akhir Jumbotron -->
+
+  <!-- Jumbotron -->
+  <div class="container">
+
+    <!-- Panel -->
+    <div class="row justify-content-center">
+      <div class="col-12 panel">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <!-- <li data-target="#carouselExampleIndicators" data-slide-to="2"></li> -->
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="row">
+                <div class="carousel-caption">
+                  <h4>AYO...!!! TERAPKAN PROTOKOL KESEHATAN 3M</h4>
+                </div>
+                <div class="col">
+                  <img class="d-block" src="assets/img/Memakai masker.jpg" alt="">
+                  <p>Menggunakan masker saat beraktivitas</p>
+                </div>
+                <div class="col">
+                  <img class="d-block" src="assets/img/Mencuci tangan.jpg" alt="">
+                  <p>Mencuci tangan memakai sabun</p>
+                </div>
+                <div class="col">
+                  <img class="d-block" src="assets/img/Jaga Jarak.jpg" alt="">
+                  <p>Menjaga jarak 1 meter</p>
+                </div>
+              </div>
+            </div>
+            <!-- <div class="carousel-item">
+              <div class="col slide1 text-center">
+                <h4>DATA KASUS COVID-19 JAWA BARAT</h4>
+              </div>
+              <div class="row">
+                <?php foreach ($data['JAWA BARAT'] as $row) {
+                ?>
+                  <div class="col slide2">
+                    <h2><?php echo $row['jumlah_kasus']; ?></h2>
+                    <h4>Orang</h4>
+                    <h5>Jumlah kasus</h5>
+                  </div>
+                  <div class="col slide3">
+                    <h2><?php echo $row['jumlah_sembuh'] ?></h2>
+                    <h4>Orang</h4>
+                    <h5>Total kasus sembuh</h5>
+                  </div>
+                  <div class="col slide4">
+                    <h2><?php echo $row['jumlah_meninggal'] ?></h2>
+                    <h4>Orang</h4>
+                    <h5>Total kasus meninggal</h5>
+                  </div>
+                <?php
+                }
+                ?>
+              </div>
+            </div> -->
+            <div class="carousel-item">
+              <div class="col slide5">
+                <img class="d-block" src="assets/img/img18.jpg" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Akhir Panel -->
+
+    <!-- Who we are -->
+    <section class="whoweare">
+      <div class="row justify-content-center">
+        <div class="col-9">
+          <h4>“Who We Are”</h4>
+          <h5>Kami akan memberikan pelayanan seputar diet sehat dengan layanan<br>
+            tips menurukan berat badan dan menu - menu diet yang sangat menarik,<br>
+            sehingga anda dengan mudah melakukan diet secara teratur untuk menjaga<br>
+            pola makan & pola hidup sehat.</h5>
+        </div>
+      </div>
+    </section>
+    <!-- Akhir Who we are -->
+
+    <!-- gallery -->
+    <div class="row gallery">
+      <div class="col-lg-4" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="400">
+        <img class="img-responsive" src="assets/img/Gambar2.jpg" alt="Gambar2" class="img-fluid">
+      </div>
+      <div class="col-lg-4" data-aos="fade-right" data-aos-duration="1000" data-aos-delay="400">
+        <img class="img-responsive" src="assets/img/Gambar3.jpg" alt="Gambar3" class="img-fluid" width="370px">
+      </div>
+      <div class="col" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="400">
+        <img class="img-responsive" src="assets/img/Gambar5.jpg" alt="Gambar5" class="img-fluid" width="340px">
+      </div>
+    </div>
+    <div class="row gallerygambar4">
+      <div class="col-lg-4 gallerygambar4" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
+        <img class="img-responsive" src="assets/img/Gambar4.jpg" alt="Gambar4" class="img-fluid" width="722px" height="290px">
+      </div>
+      <!-- <div class="col-lg-5 gallerygambar6">
+        <img class="img-responsive" src="assets/img/Gambar6.jpg" alt="Gambar6" class="img-fluid" width="250px">
+      </div> -->
+    </div>
+    <!-- Akhir gallery -->
+
+    <!-- Tips space -->
+    <div class="row">
+      <div class="col tipsspace">
+        <div class="col-lg-12">
+          <img class="img-responsive" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="400" src="assets/img/Gambar8.jpg" alt="Gambar8" class="rounded" width="400" height="550">
+        </div>
+      </div>
+    </div>
+    <!-- Akhir Tips space -->
+
+    <!-- Tips text -->
+    <div class="tipstext">
+      <div class="col">
+        <h2>Rasakan Pengalaman Saat anda <br> Mendapatkan tips menurunkan <br> berat badan.</h2>
+      </div>
+      <div class="col">
+        <h5>Tips menurunkan berat badan dari kami <br> berbeda dengan yang lain <br>
+          dan mempunyai varian menu diet <br> beragam, unik, dan menarik.</h5>
+      </div>
+      <br>
+      <div class="col">
+        <h4>Tahukah Kamu ? <br> Diet sangat bermanfaat</h4>
+        <br>
+        <p class="text-lg">1. Menurunkan Risiko Kematian Akibat Penyakit <br> Jantung Koroner.<br>
+          2. Menurunkan Risiko Kanker Usus Besar.<br>
+          3. Menurunkan Risiko Diabetes Melitus dan <br> Komplikasinya.<br>
+          4. Menurunkan Risiko Obesitas.<br>
+          5. Menurunkan Kadar Kolesterol Jahat (LDL).<br>
+          6. Awet Muda.</p>
+      </div>
+    </div>
+    <!-- Akhir Tips text -->
+
+    <!-- Testimonials -->
+    <div class="testimonials">
+      <div class="row">
+        <div class="col-md-12">
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <div class="row">
+                  <div class="col-md-4">
+                    <div class="single-box">
+                      <div class="img-area"><img src="assets/img/images.jpg" alt="" width="100%" height="100%"></div>
+                      <div class="img-text">
+                        <h2>Moh. Alfan</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus recusandae veniam nulla consequatur praesentium
+                          tempore tempora quisquam cum, quas ex provident et voluptas dolores iusto explicabo qui autem reiciendis exercitationem!</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="single-box">
+                      <div class="img-area"><img src="assets/img/images.jpg" alt="" width="100%" height="100%"></div>
+                      <div class=" img-text">
+                        <h2>Rendi Gunawan</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus repellat, aut temporibus numquam error sint, officia quas
+                          facilis perspiciatis, hic aliquam odio rem iure mollitia laboriosam ut inventore expedita ullam.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="single-box">
+                      <div class="img-area"><img src="assets/img/images.jpg" alt="" width="100%" height="100%"></div>
+                      <div class="img-text">
+                        <h2>Aldi Julfiansyah</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis ullam non alias perspiciatis odio omnis
+                          facilis, blanditiis obcaecati cum ex voluptas accusantium ipsa porro similique cupiditate. Ab, voluptatibus velit.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="row">
+                  <div class="col-md-4">
+                    <div class="single-box">
+                      <div class="img-area"><img src="assets/img/images.jpg" alt="" width="100%" height="100%"></div>
+                      <div class="img-text">
+                        <h2>M. Erlanda Tubagus H</h2>
+                        <p>This card has supporting text below as a natural lead-in to additional content.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="single-box">
+                      <div class="img-area"><img src="assets/img/images.jpg" alt="" width="100%" height="100%"></div>
+                      <div class="img-text">
+                        <h2>Nouval Akmal S</h2>
+                        <p>This card has supporting text below as a natural lead-in to additional content.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="single-box">
+                      <div class="img-area"><img src="assets/img/images.jpg" alt="" width="100%" height="100%"></div>
+                      <div class="img-text">
+                        <h2>M. Aji Pamungkas</h2>
+                        <p>This card has supporting text below as a natural lead-in to additional content.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Akhir testimonials -->
+
+    <!-- Promotion -->
+    <div class="promotion" id="promotion">
+      <div class="col">
+        <div class="col">
+          <img class="img-responsive" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="200" src="assets/img/Gambar9.jpg" alt="Gambar9" class="rounded">
+        </div>
+        <div class="col">
+          <h4 data-aos="fade-right" data-aos-duration="1000">Dapatkan tips menurunkan <br> berat badan dan menu diet <br> gratis. Hanya untuk kamu!!!</h4>
+        </div>
+        <form class="d-flex">
+          <input class="form-control me-2" type="text" placeholder="     Your Email Addres. . ." aria-label="Your Email Addres">
+          <button type="email" class="btn ">Submit</button>
+        </form>
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
+  <!-- Akhir promotion -->
+
+
+  <!-- Footer -->
+  <div id="tentangkami">
+    <div class="container">
+      <div class="row footer">
+        <div class="col-lg-5 text1"><br>
+          <a href="#">Healty DietStyle</a>
+          <p>Gaya hidup sehat adalah tempat untuk anda <br> bisa peduli kepada kesehatan tubuh anda dan <br> akan
+            membawa
+            kebahagiaan di masa yang akan datang.</p>
+        </div>
+        <div class="col-lg-2 text2">
+          <h5>Support</h5>
+          <a href="">Tentang Kami</a><br>
+          <a href="">FAQ</a><br>
+          <a href="">Kebijakan Pribadi</a><br>
+          <a href="">Keamanan</a><br>
+        </div>
+        <div class="col-lg-2 text3">
+          <h5>Pages</h5>
+          <a href="">Help</a><br>
+          <a href="">Kontak</a><br>
+        </div>
+        <div class="col-lg-2 text4">
+          <h5>Resource</h5>
+          <a href="">Dokumentasi</a>
+        </div>
+      </div>
+
+      <div class="row footerbawah">
+        <div class="row fotbar">
+          <div class="col-lg-4 icon2">
+            <i class="fab fa-facebook"></i>
+            <i class="fab fa-instagram ml-5"></i>
+            <i class="fab fa-twitter ml-5"></i>
+          </div>
+          <div class="col-lg-6 text5">
+            <h5>© 2021 All Rights Reserved Healty Dietestyle. </h5>
+          </div>
+          <div class="col-lg-2 text6">
+            <h5>Terms of Service.</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Akhir footer -->
+  </div>
+  <!-- Akhir Jumbotron -->
+
+  <!-- Optional JavaScript -->
+  <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+  </script>
+
+</body>
+
+</html>
